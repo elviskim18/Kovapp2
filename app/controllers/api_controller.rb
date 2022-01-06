57dc3ca421ebd57dd6cef0a4a47dc76e0d7874a4
Chris <nyagachris75@gmail.com>
2022-01-06 18:00:36 +0300

    add comments

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,8 +8,11 @@ class ApiController < ApplicationController
 
   def verify
     qr = params[:qr_code]
+    # we fetch out secret key from out application secrets
     secret_key = Rails.application.secret_key_base
+    # we decrypt and verify the qr code using the secret key
     crypt = ActiveSupport::MessageEncryptor.new(secret_key[0..31], secret_key)
+    # if the verification fails, and exception will be raised which is handled in line 26
     crypt.decrypt_and_verify(qr)
     # beyond here, we are sure the the qr code is valid
     certificate = Certificate.includes(:user).find_by(qr_code: qr)
@@ -20,6 +23,7 @@ class ApiController < ApplicationController
       email: user.email,
       national_id: user.national_id
     }
+    # when the qr code is invalid we return an invalid message that is sent to the mobile app
     rescue
       render json: { error: 'Invalid QR Code' }, status: 400
   end
